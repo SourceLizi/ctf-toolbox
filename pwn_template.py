366a811175c50a9b6e3e4c1be29f9a72bcb231b0
Lizi <30660118+SourceLizi@users.noreply.github.com>
2020-08-07 21:59:30 +0800

    Add files via upload

---
+++ pwn_template.py
@@ -0,0 +1,12 @@
+from pwn import *
+
+#r=process("./pwn") #local file
+#r=remote("ip_address",port_num) #remote server
+
+#payload=...
+#payload='A'*32+p32(addr)
+
+#r.recvline()
+#r.sendline(payload)
+
+#r.interactive()
